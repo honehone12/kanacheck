e8e9816cbe0b55e4a0c77e98393fe222228f8c99
Twelfthonehone <honehone.41games@gmail.com>
2025-10-13 18:30:10 +0900

    added clean

--- run.sh
+++ run.sh
@@ -2,10 +2,31 @@
 
 set -e
 
+USAGE="use clean|run"
+ 
+if [ -z "$1" ]; then
+    echo $USAGE
+    exit 1
+fi
+
+SUBCMD="$1"
+
 cd kanacheck
 
-mvn package
-echo "******"
-java -jar target/kanacheck-1.0-SNAPSHOT-jar-with-dependencies.jar
+case "$SUBCMD" in
+    "clean")
+        mvn clean
+        ;;
+    "run")
+        mvn package
+        echo "******"
+        java -jar target/kanacheck-1.0-SNAPSHOT-jar-with-dependencies.jar
+        ;;
+    *)
+        echo $USAGE
+        exit 1
+        ;;
+esac
 
 cd ..
+exit 0
