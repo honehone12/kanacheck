853b665e5e18172def557950e7970504fc3ba9d5
Twelfthonehone <honehone.41games@gmail.com>
2025-10-14 16:32:41 +0900

    remove static

--- kanacheck/src/main/java/com/kanacheck/Kanacheck.java
+++ kanacheck/src/main/java/com/kanacheck/Kanacheck.java
@@ -1,14 +1,17 @@
 package com.kanacheck;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import tools.jackson.databind.ObjectMapper;
 import com.kanacheck.config.Config;
 
-public class Kanacheck { 
-    private final Logger _log = LogManager.getLogger(Kanacheck.class); 
+public class Kanacheck {
+    private final Path CONFIG_PATH = Paths.get("kanacheck.json");
+    private final Logger LOG = LogManager.getLogger(Kanacheck.class); 
 
     public void config() {
         try {
@@ -16,18 +19,48 @@ public class Kanacheck {
             final var ser = new ObjectMapper();
             final var json = ser.writerWithDefaultPrettyPrinter()
                 .writeValueAsString(config);
-            final var path = Paths.get("kanacheck.json");
-            Files.writeString(path, json);
-            _log.info("config file has been generated as " + path);
+            Files.writeString(CONFIG_PATH, json);
+            LOG.info("Config file has been generated as " + CONFIG_PATH);
         } catch (Exception e) {
-            _log.error(e);
+            LOG.error(e);
         }
     }
 
     public void check(boolean recursive, String path) {
         if (path == null) {
-            _log.error("Please provide path");
+            LOG.error("Please provide path");
             return;
         }
+
+        try {
+            if (!Files.exists(CONFIG_PATH)) {
+                LOG.error("Please config first (--config)");
+                return;
+            }
+
+            final var config = readConfig();
+            if (recursive) {
+                searchDir(config);
+            } else {
+                searchFile(config);
+            }
+        } catch (Exception e) {
+            LOG.error(e);
+        }
+    }
+
+    private Config readConfig() throws IOException {
+        final var json = Files.readString(CONFIG_PATH);
+        final var de = new ObjectMapper();
+        final var config = de.readValue(json, Config.class);
+        return config;
+    }
+
+    private void searchFile(Config config) {
+
+    }
+
+    private void searchDir(Config config) {
+
     }
 }
